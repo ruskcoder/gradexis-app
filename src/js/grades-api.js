cc25c15f447806bc69b2fe2b72809fe3fcd350fd
ruskcoder <118460956+ruskcoder@users.noreply.github.com>
2025-03-21 19:20:02 -0500

    Update grades-api.js

--- src/js/grades-api.js
+++ src/js/grades-api.js
@@ -1,8 +1,8 @@
 import store from "./store.js";
 import terminal from 'virtual:terminal';
 
-// const apiUrl = 'https://api.gradexis.com';
-const apiUrl = 'https://supreme-trout-w6vv69pgppx3p4p-3000.app.github.dev';
+ const apiUrl = 'https://api.gradexis.com';
+//const apiUrl = 'https://supreme-trout-w6vv69pgppx3p4p-3000.app.github.dev';
 
 const platformList = ['hac']
 function updateSession(data) {
