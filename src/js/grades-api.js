31620d76b0c65766bc64989e5af40b65378a034c
ruskcoder <118460956+ruskcoder@users.noreply.github.com>
2025-03-22 16:30:16 +0000

    Api & Dragging

--- src/js/grades-api.js
+++ src/js/grades-api.js
@@ -1,8 +1,10 @@
 import store from "./store.js";
 import terminal from 'virtual:terminal';
 
- const apiUrl = 'https://api.gradexis.com';
-//const apiUrl = 'https://supreme-trout-w6vv69pgppx3p4p-3000.app.github.dev';
+const apiUrl = 'https://supreme-trout-w6vv69pgppx3p4p-3000.app.github.dev';
+if (location.host == "mobile.gradexis.com") { 
+    const apiUrl = 'https://api.gradexis.com'; 
+}
 
 const platformList = ['hac']
 function updateSession(data) {
@@ -24,7 +26,7 @@ export async function login(loginData) {
     } catch (error) {
         return { success: false, message: error.message };
     }
-} 
+}
 
 export async function getClasses(term = null) {
     // return {
