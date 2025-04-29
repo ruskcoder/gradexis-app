7ede4ba095b542a9acc0a99eb698eaef2a9fab33
0xc60f <0xc60f@duck.com>
2025-04-28 20:03:37 -0500

    App ready to publish

---
+++ ios/App/Pods/Target Support Files/HugotomaziCapacitorNavigationBar/HugotomaziCapacitorNavigationBar-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_HugotomaziCapacitorNavigationBar : NSObject
+@end
+@implementation PodsDummy_HugotomaziCapacitorNavigationBar
+@end
--- src/pages/settings.jsx
+++ src/pages/settings.jsx
@@ -524,7 +524,7 @@ const SettingsPage = ({ f7router }) => {
                 ios="f7:person_circle"
                 md="material:group"
               ></Icon>
-              <span>Acount Manager</span>
+              <span>Account Manager</span>
             </ListItem>
           </List>
         </CardContent>
